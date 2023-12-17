d40f6db524a7cec2b4cb042cbbed11c9cd9f99b6
Luiz Guilherme <88911920+luizgmelo@users.noreply.github.com>
2023-12-17 16:41:23 -0300

    connect api and content

---
+++ script.js
@@ -0,0 +1,31 @@
+const app = document.getElementById('root')
+const container = document.createElement('div')
+container.setAttribute('class', 'container')
+app.appendChild(container)
+
+let request = new XMLHttpRequest()
+
+request.open('GET', 'https://narutodb.xyz/api/akatsuki')
+
+request.onload = function () {
+  let data = JSON.parse(this.response)["akatsuki"];
+  data.forEach( (character) => {
+    let card = document.createElement('div')
+    card.setAttribute('class', 'card')
+    
+    let h2 = document.createElement('h2')
+    h2.setAttribute('class', 'title')
+    h2.textContent = character['name']
+
+    let img = document.createElement('img')
+    img.setAttribute('class', 'image')
+    img.setAttribute('src', character['images'][0]
+)
+    container.appendChild(card)
+
+    card.appendChild(h2)
+    card.appendChild(img)
+  });
+}
+
+request.send()
